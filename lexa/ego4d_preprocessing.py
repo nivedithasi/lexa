e9fde03f7e7116e210d5521fdff893ba71f1c721
Suraj Nair <surajn@iris-ws-18.stanford.edu>
2022-10-20 14:23:21 -0700

    added multiprocessing to ego4d parsing

--- lexa/ego4d_preprocessing.py
+++ lexa/ego4d_preprocessing.py
@@ -10,51 +10,37 @@ from torchvision import transforms
 import torchvision
 import pandas as pd
 import os.path
+import time
+
+from multiprocessing import Pool
+
+
+
+
 
 f = open(path)
-  
 # returns JSON object as 
 # a dictionary
 data = json.load(f)
-
-
-data_logger = []
 ego_path = "/iris/u/nivsiyer/ego4d/"
-
 resize_transform = transforms.Compose([
     transforms.Resize((64, 64)),
     # transforms.CenterCrop(224),
     transforms.ToTensor()]
 )
 
-numvids = len(data['videos'])
-for i, video in enumerate(data['videos']):
-    print("*" * 50)
-    print("Video", video['video_uid'], f"{i} of {numvids}")
-    
+def process_vid(video):
+    t0 = time.time()
     video_name = "/nlp/scr2/nlp/ego4d/data/raw/ego4d/v1/full_scale/" + video['video_uid'] + '.mp4'
     try: 
         reader = av.open(video_name)
     except:
         print("Issue with opening the video path:", video_name)
-        assert(False)
-        
-    # Convert video into a list of images
-    # images = []
-    
-    vidpath = ego_path + "videos" + "/" + video['video_uid']
-    os.makedirs(vidpath, exist_ok=True)
-#     for idx, frame in enumerate(reader.decode(video=0)):
-#         if os.path.isfile(vidpath + "/" + f"{str(idx).zfill(7)}.jpg"):
-#           continue
-        
-#         f = frame.to_image()
-#         f = resize_transform(f)
-#         # images.append(f)
-#         torchvision.utils.save_image(f, vidpath + "/" + f"{str(idx).zfill(7)}.jpg")
+        return {}
+
+    vidpath = ego_path + "videos2" + "/" + video['video_uid']
 
     clips = video['clips']
-    # print(clips)
     manifest = {}
     manifest["label"] = []
     manifest["vidpath"] = []
@@ -62,10 +48,8 @@ for i, video in enumerate(data['videos']):
     manifest["end_frame"] = []
     
     allvidframes = []
-    
     numclips = len(clips)
     for e, clip in enumerate(clips):
-        print(f"Clip {e} of {numclips}")
         annotations = clip['annotations']
         for annot in annotations:
             labels = annot['labels']
@@ -76,7 +60,6 @@ for i, video in enumerate(data['videos']):
                 end_frame = int(label['video_end_frame'])
                 allvidframes += list(range(start_frame, end_frame+1))
                 task_name = label['label']
-                print(task_name, start_frame, end_frame)
                 try:
                   manifest["label"].append(task_name)
                   manifest["vidpath"].append(vidpath)
@@ -84,17 +67,43 @@ for i, video in enumerate(data['videos']):
                   manifest["end_frame"].append(str(end_frame).zfill(7))
                 except:
                   pass
+           
+    if os.path.isdir(vidpath):
+      print(f"Video {video['video_uid']} already Done, Process time {time.time() - t0}")
+      return manifest
+    os.makedirs(vidpath, exist_ok=True)
                 
     allvidframes = set(allvidframes)
-    
     for idx, frame in enumerate(reader.decode(video=0)):
         if os.path.isfile(vidpath + "/" + f"{str(idx).zfill(7)}.jpg") or (idx not in allvidframes):
           continue
-        
         f = frame.to_image()
         f = resize_transform(f)
         torchvision.utils.save_image(f, vidpath + "/" + f"{str(idx).zfill(7)}.jpg")
-    
-df = pd.DataFrame.from_dict(manifest)
+    print(f"Video {video['video_uid']} Done, Process time {time.time() - t0}")
+    return manifest
+        
+
+with Pool(4) as p:
+  mfs = p.map(process_vid, data['videos'])
+
+finalmanifest = {}
+finalmanifest["label"] = []
+finalmanifest["vidpath"] = []
+finalmanifest["start_frame"] = []
+finalmanifest["end_frame"] = []
+for mf in mfs:
+  if mf == {}:
+    continue
+  if len(mf["label"]) == 0:
+    continue
+  assert(len(mf["label"]) == len(mf["vidpath"]) == len(mf["start_frame"]) == len(mf["end_frame"]))
+  
+  finalmanifest["label"] += mf["label"]
+  finalmanifest["vidpath"] += mf["vidpath"]
+  finalmanifest["start_frame"] += mf["start_frame"]
+  finalmanifest["end_frame"] += mf["end_frame"]
+        
+df = pd.DataFrame.from_dict(finalmanifest)
 print(df)
-df.to_csv("/iris/u/nivsiyer/ego4d/videos/manifest.csv")
+df.to_csv("/iris/u/nivsiyer/ego4d/videos2/manifest.csv")
